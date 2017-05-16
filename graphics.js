6749ab45aa41a759c03d7460f51e7c29bb6fb69c
Unknown <jbrink386@gmail.com>
2017-05-16 20:50:59 +0200

    Add Graphics File
    Some basic drawing functions

---
+++ graphics.js
@@ -0,0 +1,44 @@
+function drawFillRect(ctx, style, rect) {            
+    ctx.fillStyle = style; 
+    ctx.fillRect(rect.x, rect.y, rect.width, rect.height);                                        
+};
+
+function drawStrokeRect(ctx, style, rect, strokeWidth) {
+    ctx.strokeStyle = style;  
+    ctx.strokeWidth = strokeWidth;
+    ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+function drawFillArc(ctx, style, centerX, centerY, radius, startAngle = 0, endAngle = 2 * Math.PI) {
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.fill();
+};
+
+function drawFillText(ctx, fontFamily, fontSize, text, x, y) {
+    ctx.font = fontSize + "px" + fontFamily;        
+    ctx.fillText(text, x, y);            
+};
+
+function drawImage(ctx, image, destinationRect, sourceRect = null) {
+    if (sourceRect) {
+        ctx.drawImage(
+            image, 
+            sourceRect.x, 
+            sourceRect.y, 
+            sourceRect.width, 
+            sourceRect.height,
+            destinationRect.x, 
+            destinationRect.y, 
+            destinationRect.width, 
+            destinationRect.height);
+    }            
+    else {
+        ctx.drawImage(
+            image, 
+            destinationRect.x, 
+            destinationRect.y, 
+            destinationRect.width, 
+            destinationRect.height);
+    }            
+};
