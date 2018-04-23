31aff722557bf65c49c708a441c4aeeecd0119a4
Unknown <jbrink386@gmail.com>
2018-04-23 22:05:02 +0200

    feat: added text rendering functionality

--- example.js
+++ example.js
@@ -16,6 +16,17 @@ var ctx = canvas.getContext("2d");
 var delta = 0;
 var deltaTime = Date.now();
 
+var titleTextStyle = "palegoldenrod";
+var titleShadowTextStyle = "black";
+var titleTextX = 100;
+var titleTextY = 100;
+var titleShadowTextX = 100;
+var titleShadowTextY = 100;
+var titleText = "Animated Backgrounds";
+var titleFontSize = 60;
+var titleShadowFontSize = 60;
+
+
 var image = new Image();
 //var animation;
 //var animationDestination = new Rect(340, 350, 100, 125);
@@ -51,6 +62,9 @@ Loop();
 
 function PopulateParalaxBackgrounds() {
 
+    // paralaxBackgroundSlider.Backgrounds.push(
+    //     new ParalaxBackground(3, new Rect(400, -200, 768, 1024), 1, 0.35));
+
     paralaxBackgroundSlider.Backgrounds.push(
         new ParalaxBackground(1, new Rect(200, 0, 512, 512), 1, 0.01));    
 
@@ -58,7 +72,7 @@ function PopulateParalaxBackgrounds() {
         new ParalaxBackground(2, new Rect(0, 150, 384, 384), 1, 0.02));    
     
     paralaxBackgroundSlider.Backgrounds.push(
-        new ParalaxBackground(3, new Rect(400, 250, 256, 256), 1, 0.03));    
+        new ParalaxBackground(3, new Rect(400, 250, 256, 256), 1, 0.03));             
 };
 
 
@@ -81,6 +95,19 @@ function UpdateDelta() {
 
 function Update() {    
     paralaxBackgroundSlider.Update(bounds, delta);    
+
+    UpdateTitleTextPosition();
+};
+
+function UpdateTitleTextPosition() {
+    // TODO:
+    // Calculate all text movenment here.
+
+
+    titleTextX = 20;
+    titleTextY = 100;
+    titleShadowTextX = titleTextX + 5;
+    titleShadowTextY = titleTextY + 5;
 };
 
 function Draw() {    
@@ -92,6 +119,30 @@ function Draw() {
 
     //drawFillRect(ctx, "green", {x:bounds.X, y:bounds.Y, width:bounds.W, height:bounds.H});
     paralaxBackgroundSlider.Draw(ctx); 
+    
+    DrawTextOverlay();
+};
+
+function DrawTextOverlay() {
+    DrawTitleText(titleShadowTextStyle, titleShadowTextX, titleShadowTextY, titleText, titleShadowFontSize);
+    DrawTitleText(titleTextStyle, titleTextX, titleTextY, titleText, titleFontSize);   
+
+    DrawTitleText("black", 75, 250, "todo: animate text!", 40);
+    DrawTitleText("skyblue", 80, 255, "todo: animate text!", 40);
+
+    DrawTitleText("black", 75, 290, "todo: create todo style menu!", 40);
+    DrawTitleText("skyblue", 80, 295, "todo: create todo style menu!", 40);
+}
+
+function DrawTitleText(style, x, y, text, fontSize) {
+    drawFillText(
+        ctx, 
+        style, 
+        "Showcard Gothic", 
+        fontSize, 
+        text, 
+        x, 
+        y);
 };
 
 
