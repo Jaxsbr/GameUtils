60245fe822716f9a323c29aa47f9f951a02078d3
Unknown <jbrink386@gmail.com>
2018-04-27 12:59:13 +0200

    added point and rectangle methods

--- js/point.js
+++ js/point.js
@@ -1,6 +1,19 @@
 class Point {
     constructor(x, y) {
         this.X = x;
-        this.Y = y;    
+        this.Y = y;            
+    }
+
+    DistanceBetween(point) {
+        let x = this.X - point.X;
+        let y = this.Y - point.Y;
+        return Math.sqrt(x * x - y * y);
+    }
+
+    Normalize(point) {
+        let x = this.X - point.X;
+        let y = this.Y - point.Y;
+        let distance = Math.sqrt(x * x - y * y);
+        return new Point(x / distance, y / distance);
     }
 }
--- js/rectangle.js
+++ js/rectangle.js
@@ -4,5 +4,37 @@ class Rectangle {
         this.Y = y;
         this.W = w;
         this.H = h;
+
+        this.Left = this.X;
+        this.Top = this.Y;
+        this.Right = 0;
+        this.Botton = 0;
+        this.Centre = new Point(0, 0);
+
+        this.Update();
+    }
+
+    Update() {
+        this.Left = this.X;
+        this.Top = this.Y;
+        this.Right = this.Left + this.W;
+        this.Botton = this.Top + this.H;
+
+        this.Centre.X = this.X + (this.W / 2);
+        this.Centre.Y = this.Y + (this.H / 2);
+    }
+
+    Intersect(rectangle) {
+        return !(rectangle.Left > this.Right ||
+                rectangle.Right < this.Left ||
+                rectangle.Left > this.Bottom ||
+                rectangle.Bottom < this.Top);
+    }
+
+    Contains(rectangle) {
+        return (this.Left <= rectangle.Left &&
+                rectangle.Right <= this.Right &&
+                this.Top <= rectangle.Top &&
+                rectangle.Bottom <= this.Bottom);
     }
 }
